787913b87804be430aa365f43dbca6041a289e82
kevnhech <kevnhech@gmail.com>
2024-05-09 16:05:03 -0700

    Write a function that can display books

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const defaultDomState = document.body.innerHTML;
+const body = document.querySelector("body");
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -21,4 +23,35 @@ function addBookToLibrary() {
   let bookRead = prompt("Have you read it?");
   let userBook = new Book(bookTitle, bookAuthor, bookPages, bookRead);
   myLibrary.push(userBook);
+
+  displayLibrary();
 }
+
+function displayLibrary() {
+  document.body.innerHTML = defaultDomState;
+  for (let i = 0; i < myLibrary.length; i++) {
+    let card = document.createElement("div");
+    let bookTitle = document.createElement("p");
+    let bookAuthor = document.createElement("p");
+    let bookPages = document.createElement("p");
+    let bookInfo = document.createElement("p");
+
+    bookTitle.textContent = myLibrary[i].title;
+    bookAuthor.textContent = myLibrary[i].author;
+    bookPages.textContent = myLibrary[i].pages;
+    bookInfo.textContent = myLibrary[i].info();
+
+    card.appendChild(bookTitle);
+    card.appendChild(bookAuthor);
+    card.appendChild(bookPages);
+    card.appendChild(bookInfo);
+
+    body.appendChild(card);
+  }
+}
+
+// The Hobbit, J.R.R. Tolkien, 295
+// Dracula, Bram Stoker, 418
+// The Metamorphosis, Franz Kafka, 70
+
+// Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”.
