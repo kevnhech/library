03d200b9800c67141c10784659f473172dcdae50
kevin <kevnhech@gmail.com>
2024-05-16 01:52:50 -0400

    Add a remove button on each book

--- script.js
+++ script.js
@@ -37,20 +37,33 @@ function displayLibrary() {
     let bookAuthor = document.createElement("p");
     let bookPages = document.createElement("p");
     let bookInfo = document.createElement("p");
+    let removeBtn = document.createElement("button");
 
     bookTitle.textContent = myLibrary[i].title;
     bookAuthor.textContent = myLibrary[i].author;
-    bookPages.textContent = myLibrary[i].pages;
+    bookPages.textContent = `${myLibrary[i].pages} pages`;
     bookInfo.textContent = myLibrary[i].info();
+    removeBtn.textContent = "Remove"
 
     card.appendChild(bookTitle);
     card.appendChild(bookAuthor);
     card.appendChild(bookPages);
     card.appendChild(bookInfo);
+    card.appendChild(removeBtn);
 
     flexbox.appendChild(card);
+
+    removeBtn.addEventListener("click", function() {
+      myLibrary.splice(myLibrary.indexOf(myLibrary[i]), 1);
+      flexbox.removeChild(flexbox.childNodes[i]);
+    });
   }
 }
+
+// add an event listener for remove buttons that removes the current book from the array and webpage
+
+// Add a button on each book's display to remove the book from the library.
+// You will need to associate your DOM elements with the actual book objects in some way. One easy solution is giving them a data-attribute that corresponds to the index of the library array.
 
 // The Hobbit, J.R.R. Tolkien, 295
 // Dracula, Bram Stoker, 418
