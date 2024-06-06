608bae3544f0beead5a2db4d465e6a674026668f
kevin <kevnhech@gmail.com>
2024-06-06 14:34:12 -0700

    Refactor functions to a class

--- script.js
+++ script.js
@@ -1,85 +1,86 @@
 const myLibrary = [];
 const flexbox = document.querySelector(".flexbox");
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 
-function addBookToLibrary() {
-  let bookTitle = prompt("What is the title of the book?");
-  let bookAuthor = prompt("Who is the author?");
-  let bookPages = prompt("How many pages?");
-  let bookRead = prompt("Have you read it?");
-  let userBook = new Book(bookTitle, bookAuthor, bookPages, bookRead);
-  myLibrary.push(userBook);
+  static addBook() {
+    this.title = prompt("What is the title of the book?");
+    this.author = prompt("Who is the author?");
+    this.pages = prompt("How many pages?");
+    this.read = prompt("Have you read it?");
+    myLibrary.push(new Book(this.title, this.author, this.pages, this.read));
 
-  displayLibrary();
-}
+    Book.displayLibrary();
+  }
 
-function displayLibrary() {
-  for (let i = 0; i < myLibrary.length; i++) {
-    if (i != myLibrary.length - 1) {
-      continue;
-    }
-
-    let card = document.createElement("div");
-    let bookTitle = document.createElement("p");
-    let bookAuthor = document.createElement("p");
-    let bookPages = document.createElement("p");
-    let buttonRow = document.createElement("div");
-    let bookRead = document.createElement("button");
-    let removeBtn = document.createElement("button");
-
-
-    card.setAttribute("class", "book");
-    bookTitle.style.fontWeight = "700";
-
-    bookTitle.textContent = myLibrary[i].title;
-    bookAuthor.textContent = myLibrary[i].author;
-    bookPages.textContent = `${myLibrary[i].pages} pages`;
-    removeBtn.textContent = "Remove";
-
-    if (myLibrary[i].read == "yes") {
-      bookRead.textContent = "Read";
-      bookRead.style.backgroundColor = "lime"
-      bookRead.style.color = "black"
-    } else {
-      bookRead.textContent = "Not Read";
-      bookRead.style.backgroundColor = "red";
-      bookRead.style.color = "white";
-    }
-
-    buttonRow.appendChild(bookRead);
-    buttonRow.appendChild(removeBtn);
-
-    card.appendChild(bookTitle);
-    card.appendChild(bookAuthor);
-    card.appendChild(bookPages);
-    card.appendChild(buttonRow);
-
-    flexbox.appendChild(card);
-
-    bookRead.addEventListener("click", () => {
-      if (bookRead.textContent == "Read") {
-        bookRead.textContent = "Not Read"
-        myLibrary[Array.from(flexbox.childNodes).indexOf(card)].read = 'no';
-        bookRead.style.backgroundColor = "red";
-        bookRead.style.color = "white";
-      } else {
-        bookRead.textContent = "Read"
-        myLibrary[Array.from(flexbox.childNodes).indexOf(card)].read = 'yes';
+  static displayLibrary() {
+    for (let i = 0; i < myLibrary.length; i++) {
+      if (i != myLibrary.length - 1) {
+        continue;
+      }
+  
+      let card = document.createElement("div");
+      let bookTitle = document.createElement("p");
+      let bookAuthor = document.createElement("p");
+      let bookPages = document.createElement("p");
+      let buttonRow = document.createElement("div");
+      let bookRead = document.createElement("button");
+      let removeBtn = document.createElement("button");
+  
+  
+      card.setAttribute("class", "book");
+      bookTitle.style.fontWeight = "700";
+  
+      bookTitle.textContent = myLibrary[i].title;
+      bookAuthor.textContent = myLibrary[i].author;
+      bookPages.textContent = `${myLibrary[i].pages} pages`;
+      removeBtn.textContent = "Remove";
+  
+      if (myLibrary[i].read == "yes") {
+        bookRead.textContent = "Read";
         bookRead.style.backgroundColor = "lime"
         bookRead.style.color = "black"
+      } else {
+        bookRead.textContent = "Not Read";
+        bookRead.style.backgroundColor = "red";
+        bookRead.style.color = "white";
       }
-    });
-
-    removeBtn.addEventListener("click", () => {
-      myLibrary.splice(Array.from(flexbox.childNodes).indexOf(card), 1);
-      flexbox.removeChild(card);
-    });
+  
+      buttonRow.appendChild(bookRead);
+      buttonRow.appendChild(removeBtn);
+  
+      card.appendChild(bookTitle);
+      card.appendChild(bookAuthor);
+      card.appendChild(bookPages);
+      card.appendChild(buttonRow);
+  
+      flexbox.appendChild(card);
+  
+      bookRead.addEventListener("click", () => {
+        if (bookRead.textContent == "Read") {
+          bookRead.textContent = "Not Read"
+          myLibrary[Array.from(flexbox.childNodes).indexOf(card)].read = 'no';
+          bookRead.style.backgroundColor = "red";
+          bookRead.style.color = "white";
+        } else {
+          bookRead.textContent = "Read"
+          myLibrary[Array.from(flexbox.childNodes).indexOf(card)].read = 'yes';
+          bookRead.style.backgroundColor = "lime"
+          bookRead.style.color = "black"
+        }
+      });
+  
+      removeBtn.addEventListener("click", () => {
+        myLibrary.splice(Array.from(flexbox.childNodes).indexOf(card), 1);
+        flexbox.removeChild(card);
+      });
+    }
   }
 }
 
