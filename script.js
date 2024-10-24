495c7a0f4cce85d50b954ff81350684c4146a80d
kevin <kevnhech@gmail.com>
2024-10-24 12:20:35 -0700

    Add multiple Book classes inside myLibrary array

--- script.js
+++ script.js
@@ -20,11 +20,8 @@ class Book {
   }
 
   static displayLibrary() {
+    flexbox.textContent = "";
     for (let i = 0; i < myLibrary.length; i++) {
-      if (i != myLibrary.length - 1) {
-        continue;
-      }
-  
       let card = document.createElement("div");
       let bookTitle = document.createElement("p");
       let bookAuthor = document.createElement("p");
@@ -84,6 +81,7 @@ class Book {
   }
 }
 
-// The Hobbit, J.R.R. Tolkien, 295
-// Dracula, Bram Stoker, 418
-// The Metamorphosis, Franz Kafka, 70
+myLibrary.push(new Book("The Hobbit", "J.R.R. Tolkien", "295", "no"));
+myLibrary.push(new Book("Dracula", "Bram Stoker", "418", "no"));
+myLibrary.push(new Book("The Metamorphosis", "Franz Kafka", "70", "no"));
+Book.displayLibrary();
